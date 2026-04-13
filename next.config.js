1e31fe5d97eca8c2163e3abfbfefc5e3a4a0ceae
Eliuzium <eliuzium@gmail.com>
2026-04-13 23:27:00 +0300

    fix: enable output export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',
+  images: { unoptimized: true },
 };
-
 module.exports = nextConfig;
